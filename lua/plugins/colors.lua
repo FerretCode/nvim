0d31d1865ce1827d5c26d0faa1d556ee0e6f635f
ferretcode <49322226+FerretCode@users.noreply.github.com>
2025-07-14 17:55:24 -0700

    Update colors.lua

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -1,7 +1,37 @@
 return {
-    { "tribela/transparent.nvim", event = "VimEnter", config = "true" },
     { "ellisonleao/gruvbox.nvim" },
     { "romainl/Apprentice" },
     { "diegoulloao/neofusion.nvim" },
     { "catppuccin/nvim", name = "catppuccin" },
+    {
+        "LazyVim/LazyVim",
+        opts = {
+            colorscheme = "apprentice",
+        },
+    },
+    {
+        "goolord/alpha-nvim",
+        event = "VimEnter",
+        opts = {},
+        config = function()
+            local alpha = require("alpha")
+            local dashboard = require("alpha.themes.dashboard")
+
+            dashboard.section.header.val = {
+                [[███    ██ ███████  ██████  ██    ██ ██ ███    ███]],
+                [[████   ██ ██      ██    ██ ██    ██ ██ ████  ████]],
+                [[██ ██  ██ █████   ██    ██ ██    ██ ██ ██ ████ ██]],
+                [[██  ██ ██ ██      ██    ██  ██  ██  ██ ██  ██  ██]],
+                [[██   ████ ███████  ██████    ████   ██ ██      ██]],
+            }
+
+            --[[
+            dashboard.section.buttons.val = {
+                dashboard.button("")
+            }]]
+            --
+
+            alpha.setup(dashboard.config)
+        end,
+    },
 }
